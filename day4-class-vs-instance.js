81c6123da7ae79d8786808ba15345cb51b4f6370
Ashish Nallana <ashishnallana@gmail.com>
2021-01-15 21:01:05 +0530

    Create day4-class-vs-instance.js

---
+++ day4-class-vs-instance.js
@@ -0,0 +1,26 @@
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+    let age;
+    if(initialAge >= 0){
+    age = initialAge;
+    } else if(initialAge < 0){
+        age = 0;
+        console.log("Age is not valid, setting age to 0.")
+    }
+    
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+   if(age < 13){
+       console.log("You are young.");
+   } else if(age >= 13 && age < 18){
+       console.log("You are a teenager.");
+   } else{
+       console.log("You are old.");
+   }
+
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+          age++;
+   };
+}
